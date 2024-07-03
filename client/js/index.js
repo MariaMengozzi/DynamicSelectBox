867c35389364eeb1b7dc25bf76f4e5a6c735376a
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-03 17:08:04 +0200

    feat: add dynamic filtered get from server

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,7 @@
-var options = []
-var count = 0
+var noVisOptions = 0
+var start = 0
+var end = 10
+var oldSearchLength = 0
 
 $(document).ready(function () {
 
@@ -19,25 +21,63 @@ $(document).ready(function () {
         const filter = input.val().toUpperCase()
         const li = $("li")
 
-        for (let i = 0; i < li.length; i++) {
-            txtValue = li[i].textContent || li[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
+        if (($("#myInput").val().length >=2 && noVisOptions)
+            || oldSearchLength < $("#myInput").val().length){
+            $('#myDropdown').empty()
+            query = "start=" + start + "&end=" + end + "&filter=" + $("#myInput").val()
+
+            $.ajax({
+                url: "http://127.0.0.1:8000/options?" + query,
+                type: "get",
+                success: function (result) {
+                    noVisOptions = (end -1) ==(result["opt"].length - 1)
+                    for (let index = 0; index < end-1 && index < result["opt"].length; index++) {
+                        element = result["opt"][index]
+                        $("#myDropdown").append(`<li tabindex='-1'>${element}</li>`);
+                    }
+                },
+                error: function (res) {
+                    console.log(res)
+                }
+            });
+
+            $("#myDropdown").show()
+
+        } else {
+            for (let i = 0; i < li.length; i++) {
+                txtValue = li[i].textContent || li[i].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
             }
         }
+
+        oldSearchLength = $("#myInput").val().length
+        
     });
+
+    $("#myInput").keydown(function (e) {
+        if (e.keyCode == 40) {
+            $("#myDropdown li:first-child").focus();
+            return false;
+        }
+    });
+    
 
     $("#myInput").focusin(function () {
         $('#myDropdown').empty()
+        query = "start=" + start + "&end=" + end + "&filter=" + $("#myInput").val() 
         $.ajax({
-            url: "http://127.0.0.1:8000/options", 
+            url: "http://127.0.0.1:8000/options?" + query, 
             type: "get",
             success: function (result) {
-                result["opt"].forEach(element => {
+                noVisOptions = (end - 1) == (result["opt"].length - 1)
+                for (let index = 0; index < end - 1 && index < result["opt"].length; index++) {
+                    element = result["opt"][index]
                     $("#myDropdown").append(`<li tabindex='-1'>${element}</li>`);
-                });
+                }
             },
             error: function(res){
                 console.log(res)
@@ -67,6 +107,7 @@ $(document).ready(function () {
             return false;
         }
     });
+
 
     /* $("li").on("keydown", function (e) {
 
